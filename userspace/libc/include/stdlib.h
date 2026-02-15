38b5752d8b8eefdc22a7754618950f0b228a3031
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2026-02-15 16:37:14 -0300

    Update stdlib.h

--- userspace/libc/include/stdlib.h
+++ userspace/libc/include/stdlib.h
@@ -3,6 +3,30 @@
 
 #include <stdint.h>
 
-void exit(int status);
+typedef uint64_t size_t;
+
+/* Process termination */
+void exit(int status) __attribute__((noreturn));
+
+/* Memory allocation */
+void *malloc(size_t size);
+void  free(void *ptr);
+void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+
+/* String conversion */
+int   atoi(const char *nptr);
+long  atol(const char *nptr);
+
+/* Pseudo-random */
+#define RAND_MAX 0x7fffffff
+int   rand(void);
+void  srand(unsigned int seed);
+
+/* Environment */
+void  abort(void) __attribute__((noreturn));
+
+/* Misc */
+#define NULL ((void *)0)
 
 #endif /* _STDLIB_H */
