382a7ef24ab08757a7b80db92362c4ee8f6fb697
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2026-02-15 03:44:49 -0300

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -112,6 +112,13 @@ void __attribute__((section(".text.boot"))) kmain(void) {
     cpu_init_bsp();
     #endif
 
+    #if defined(ARCH_X86_64)
+    /* ---- 2.7 Inicializar APIC y Despertar Cores ---- */
+    #include <apic.h>
+    lapic_init();   /* Inicializar Local APIC del core principal */
+    smp_init();     /* Despertar los Application Processors (APs) */
+    #endif
+
     /* ---- 3. Inicializar Memory Managers (Solo Tier 2+) ---- */
     #if ETEROS_TIER >= 2
         /* Memory Management Unit (Paging/MPU) */
