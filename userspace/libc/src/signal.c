ac1c823c529d93ecfc5a2964127eb6755e9b2df0
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2026-02-15 16:36:56 -0300

    Create signal.c

---
+++ userspace/libc/src/signal.c
@@ -0,0 +1,59 @@
+/**
+ * éterOS Mini-LibC - Signal Functions
+ * POSIX signal handling wrappers
+ */
+
+#include <signal.h>
+#include <sys/syscall.h>
+#include <errno.h>
+
+extern int errno;
+
+static inline long syscall2(long n, long a1, long a2) {
+    long ret;
+    __asm__ volatile ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2) : "rcx", "r11", "memory");
+    return ret;
+}
+
+static inline long syscall4_sig(long n, long a1, long a2, long a3, long a4) {
+    long ret;
+    register long r10 __asm__("r10") = a4;
+    __asm__ volatile ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10) : "rcx", "r11", "memory");
+    return ret;
+}
+
+sighandler_t signal(int sig, sighandler_t handler) {
+    struct sigaction act, oldact;
+    act.sa_handler = handler;
+    act.sa_flags = 0;
+    act.sa_restorer = 0;
+    act.sa_mask = 0;
+
+    long ret = syscall4_sig(SYS_rt_sigaction, sig, (long)&act, (long)&oldact, 8);
+    if (ret < 0) {
+        errno = (int)(-ret);
+        return SIG_ERR;
+    }
+    return oldact.sa_handler;
+}
+
+int sigaction(int sig, const struct sigaction *act, struct sigaction *oldact) {
+    long ret = syscall4_sig(SYS_rt_sigaction, sig, (long)act, (long)oldact, 8);
+    if (ret < 0) { errno = (int)(-ret); return -1; }
+    return 0;
+}
+
+int sigprocmask(int how, const sigset_t *set, sigset_t *oldset) {
+    long ret = syscall4_sig(SYS_rt_sigprocmask, how, (long)set, (long)oldset, 8);
+    if (ret < 0) { errno = (int)(-ret); return -1; }
+    return 0;
+}
+
+int raise(int sig) {
+    /* Get our own PID */
+    long ret_pid;
+    __asm__ volatile ("syscall" : "=a"(ret_pid) : "a"((long)SYS_getpid) : "rcx", "r11", "memory");
+    long ret = syscall2(SYS_kill, ret_pid, sig);
+    if (ret < 0) { errno = (int)(-ret); return -1; }
+    return 0;
+}
